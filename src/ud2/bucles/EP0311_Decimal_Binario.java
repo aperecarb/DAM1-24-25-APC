cdf4bd7b66b8ab36ede53c745453b02d1f997970
Alvaro aperecarb <aperecarb@gmail.com>
2024-11-06 13:37:11 +0100

    Updates

---
+++ src/ud2/bucles/EP0311_Decimal_Binario.java
@@ -0,0 +1,24 @@
+package bucles;
+
+import java.util.Scanner;
+
+public class EP0311_Decimal_Binario {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        System.out.println("Número en base decimal: ");
+        int n = sc.nextInt();
+
+        String binario = "";
+
+        while (n / 2 > 0) {
+            binario = (n % 2) + binario;
+            n = n / 2;
+        }
+        binario = n + binario;
+        
+        System.out.println(binario);
+
+        sc.close();
+    }
+}
