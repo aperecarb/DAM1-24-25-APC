be2eed0ccf96eb2767ef325398cb94de62615dbe
Alvaro aperecarb <aperecarb@gmail.com>
2024-11-08 14:05:57 +0100

    Updates

--- src/ud2/util/Util.java
+++ src/ud2/util/Util.java
@@ -67,6 +67,24 @@ public class Util {
         return notaEnTexto;
     }
 
+    public static void imprimirTrianguloRectangulo(int n) {
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j <= i; j++) {
+                System.out.print("* ");
+            }
+            System.out.println("");
+        }
+    }
+
+    public static void imprimirTrianguloRectanguloInverso(int n) {
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j <= i; j++) {
+                System.out.print("* ");
+            }
+            System.out.println("");
+        }
+    }
+
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         double notaDouble = 5.5;
