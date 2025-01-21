51ad1954321555f2f60525deef13a8442e9ef109
Alvaro aperecarb <aperecarb@gmail.com>
2025-01-21 13:44:47 +0100

    Updates

--- src/ud4/ejercicios/BuscarFilaMayorSuma.java
+++ src/ud4/ejercicios/BuscarFilaMayorSuma.java
@@ -1,5 +1,48 @@
 package ud4.ejercicios;
 
 public class BuscarFilaMayorSuma {
-    
+    public static int[] buscarFilaMayorSuma(int[][] matriz) {
+        // Caso especial: matriz de entrada nula o vacía
+        if (matriz == null || matriz.length == 0) {
+            return new int[0]; // Devolvemos un array vacío
+        }
+
+        int filaMayorSuma = 0; // Índice de la fila con mayor suma
+        int sumaMayor = Integer.MIN_VALUE; // Inicializamos con el valor mínimo posible
+
+        for (int i = 0; i < matriz.length; i++) {
+            int sumaActual = 0;
+
+            // Calculamos la suma de la fila actual
+            for (int j = 0; j < matriz[i].length; j++) {
+                sumaActual += matriz[i][j];
+            }
+
+            if (sumaActual > sumaMayor) {
+                sumaMayor = sumaActual;
+                filaMayorSuma = i;
+            }
+        }
+
+        return matriz[filaMayorSuma];
+    }
+
+    public static void main(String[] args) {
+        int[][] matriz = {
+            {1, 2, 3},
+            {4, 5, 6},
+            {7, 8, 9}
+        };
+
+        int[] filaResultado = buscarFilaMayorSuma(matriz);
+
+        if (filaResultado.length == 0) {
+            System.out.println("La matriz está vacía o es nula.");
+        } else {
+            System.out.print("Fila con mayor suma: ");
+            for (int num : filaResultado) {
+                System.out.print(num + " ");
+            }
+        }
+    }
 }
