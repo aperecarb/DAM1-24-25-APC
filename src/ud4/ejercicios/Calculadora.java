d574da3cc92113b4b6679ccfeae37fcd48983764
Alvaro aperecarb <aperecarb@gmail.com>
2025-01-30 11:10:53 +0100

    Updates

--- src/ud4/ejercicios/Calculadora.java
+++ src/ud4/ejercicios/Calculadora.java
@@ -2,6 +2,6 @@ package ud4.ejercicios;
 
 public class Calculadora {
     public static void main(String[] args) {
-
+        System.out.println("Bienvenid@ a la calculadora. Introduzca el número a continuación en función de lo que desee hacer:\n0. Sumar.\n1. Restar.\n2. Multiplicar.\n3. Dividir.");
     }
 }
