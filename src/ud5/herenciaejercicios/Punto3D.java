27f1097fb60709ca1c246280efdb028629619cf9
aperecarb <aperecarb@gmail.com>
2025-02-26 12:16:35 +0100

    Updates

---
+++ src/ud5/herenciaejercicios/Punto3D.java
@@ -0,0 +1,20 @@
+class Punto3D extends Punto {
+    private int z;
+
+    public Punto3D(int x, int y, int z) {
+        super(x, y);
+        this.z = z;
+    }
+
+    @Override
+    public double distancia(Punto otroPunto) {
+        if (otroPunto instanceof Punto3D) {
+            Punto3D otroPunto3D = (Punto3D) otroPunto;
+            int dz = this.z - otroPunto3D.z;
+            return Math.sqrt((this.x - otroPunto3D.x) * (this.x - otroPunto3D.x) +
+                             (this.y - otroPunto3D.y) * (this.y - otroPunto3D.y) +
+                             dz * dz);
+        }
+        return super.distancia(otroPunto);
+    }
+}
