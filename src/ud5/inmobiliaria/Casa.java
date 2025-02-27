a96c0d4d9bdefc63d8958d8158048c2353049030
aperecarb <aperecarb@gmail.com>
2025-02-27 12:33:51 +0100

    Updates

--- src/ud5/inmobiliaria/Casa.java
+++ src/ud5/inmobiliaria/Casa.java
@@ -1,5 +1,15 @@
 package ud5.inmobiliaria;
 
-public class Casa {
-    
+class Casa extends Inmueble {
+    private int terrenoM2;
+
+    public Casa(String direccion, int metrosCuadrados, int habitaciones, int baños, int terrenoM2) {
+        super(direccion, metrosCuadrados, habitaciones, baños);
+        this.terrenoM2 = terrenoM2;
+    }
+
+    @Override
+    public String detalle() {
+        return "Casa en " + direccion + ":\n" + metrosCuadrados + " m2, " + habitaciones + " hab, " + baños + " baños, con terreno de " + terrenoM2 + " m2.";
+    }
 }
