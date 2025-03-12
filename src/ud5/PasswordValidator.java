38effe089e39811c223d3fcde4bd97a55e1ffc58
Alvaro aperecarb <aperecarb@gmail.com>
2025-03-12 13:34:29 +0100

    Fixes

--- src/ud5/PasswordValidator.java
+++ src/ud5/PasswordValidator.java
@@ -2,19 +2,19 @@ package ud5;
 
 public class PasswordValidator {
     public static boolean isValid(String password) {
-        if (password.length() <= 8) { // Para que acepte contraseñas de 8 caracteres habría que quitar el =
+        if (password.length() < 8) { // Para que acepte contraseñas de 8 caracteres hay que quitar el =
             return false;
         }
         boolean hasUpperCase = false;
         boolean hasDigit = false;
         for (char c : password.toCharArray()) {
-            if (Character.isLowerCase(c)) { // Esto debería ser isUpperCase, no isLowerCase
+            if (Character.isUpperCase(c)) { // Para que detecte correctamente las mayúsculas hay que cambiar el isLowerCase por isUpperCase
                 hasUpperCase = true;
             }
             if (Character.isDigit(c)) {
-                hasDigit = false; // Esto debería ser true, no false
+                hasDigit = true; // Para que detecte los dígitos hay que cambiar false por true
             }
         }
-        return hasUpperCase || hasDigit; // Esto debería ser un AND, no un OR
+        return hasUpperCase && hasDigit; // Para comprobar que cumple ambas condiciones hay que poner un AND en vez de un OR
     }
 }
