2643e3f02488d27910f0a84be15d701f4fed8cab
aperecarb <aperecarb@gmail.com>
2025-03-13 12:34:57 +0100

    Updates

--- src/ud5/inmobiliaria/Inmobiliaria.java
+++ src/ud5/inmobiliaria/Inmobiliaria.java
@@ -1,6 +1,9 @@
 package ud5.inmobiliaria;
 
-abstract class Inmueble {
+import java.util.Arrays;
+import java.util.Comparator;
+
+abstract class Inmueble implements Comparable<Inmueble> {
     public String direccion;
     public int metrosCuadrados;
     public int habitaciones;
@@ -32,40 +35,96 @@ abstract class Inmueble {
         return "Inmueble en " + direccion + " (" + metrosCuadrados + " m2, " + habitaciones + " hab)";
     }
 
+    // Orden natural: alfabéticamente por dirección
+    @Override
+    public int compareTo(Inmueble otro) {
+        return this.direccion.compareToIgnoreCase(otro.direccion);
+    }
+
+    // Métodos estáticos de ordenación solicitados
+    public static void sortMetrosDesc(Inmueble[] t) {
+        Arrays.sort(t, (a, b) -> Integer.compare(b.metrosCuadrados, a.metrosCuadrados));
+    }
+
+    public static void sortHabAMetrosDesc(Inmueble[] t) {
+        Arrays.sort(t, (a, b) -> {
+            int cmp = Integer.compare(b.habitaciones, a.habitaciones);
+            if (cmp == 0) {
+                return Integer.compare(b.metrosCuadrados, a.metrosCuadrados);
+            }
+            return cmp;
+        });
+    }
+
+    public static void sortPrecioAlquilerAsc(Inmueble[] t) {
+        Arrays.sort(t, (a, b) -> Double.compare(a.precioAlquiler, b.precioAlquiler));
+    }
+
+    public static void sortPrecioVentaAsc(Inmueble[] t) {
+        Arrays.sort(t, (a, b) -> Double.compare(a.precioVenta, b.precioVenta));
+    }
+
+    // Mostrar lista de inmuebles
+    public static void mostrarInmuebles(Inmueble[] t) {
+        for (Inmueble i : t) {
+            System.out.println(i.toString());
+        }
+    }
+
     public static void main(String[] args) {
-        // 1.1. Modelo de Clases
-        // Crea un piso en una 5a planta, de 100m2, 3 habitaciones y 2 baños en la
-        // dirección “Calle Curtidoira”. Establece los precios de alquiler y de Venta.
+        // Crear los inmuebles
         Inmueble piso1 = new Piso("Calle Curtidoira", 100, 3, 2, 5);
         piso1.setPrecioAlquiler(500);
         piso1.setPrecioVenta(200000);
 
-        // Crea otros dos pisos
         Inmueble piso2 = new Piso("Praza Maior", 80, 2, 2, 3);
         piso2.setPrecioAlquiler(400);
+        piso2.setPrecioVenta(150000);
+
         Inmueble piso3 = new Piso("Calle Rosalía de Castro", 140, 4, 2, 2);
         piso3.setPrecioVenta(250000);
 
-        // Crea una casa de 250m2 construídos, 3000m2 de terreno, 4 habitaciones y 3
-        // baños en la dirección “Carretera de Marín”.
         Inmueble casa1 = new Casa("Carretera de Marín", 250, 4, 3, 3000);
         casa1.setPrecioAlquiler(800);
         casa1.setPrecioVenta(350000);
 
-        // Crea otra casa
         Inmueble casa2 = new Casa("Lapaman", 150, 3, 2, 1000);
         casa2.setPrecioVenta(400000);
 
-        // Imprimir Inmueble: Piso, Casa
-        System.out.println(piso1);
-        System.out.println(casa1);
-        System.out.println("");
+        Inmueble[] inmuebles = { piso1, piso2, piso3, casa1, casa2 };
 
-        // Mostrar Detalle Inmueble: Piso, Casa
-        System.out.println(piso1.detalle());
-        System.out.println("");
-        System.out.println(piso2.detalle());
-        System.out.println("");
-        System.out.println(casa2.detalle());
+        // Mostrar todos los inmuebles
+        System.out.println("\nTODOS LOS INMUEBLES:");
+        mostrarInmuebles(inmuebles);
+
+        // Ordenar por dirección alfabéticamente (criterio natural)
+        Arrays.sort(inmuebles);
+        System.out.println("\nINMUEBLES ORDENADOS ALFABÉTICAMENTE:");
+        mostrarInmuebles(inmuebles);
+
+        // Ordenar por metros cuadrados ascendente
+        Arrays.sort(inmuebles, Comparator.comparingInt(i -> i.metrosCuadrados));
+        System.out.println("\nINMUEBLES ORDENADOS POR m2 (ascendente):");
+        mostrarInmuebles(inmuebles);
+
+        // Ordenar por metros cuadrados de mayor a menor
+        sortMetrosDesc(inmuebles);
+        System.out.println("\nINMUEBLES ORDENADOS POR m2 (de mayor a menor):");
+        mostrarInmuebles(inmuebles);
+
+        // Ordenar por habitaciones y luego metros cuadrados
+        sortHabAMetrosDesc(inmuebles);
+        System.out.println("\nINMUEBLES ORDENADOS POR HABITACIONES Y LUEGO m2 (de mayor a menor):");
+        mostrarInmuebles(inmuebles);
+
+        // Ordenar por precio de alquiler ascendente
+        sortPrecioAlquilerAsc(inmuebles);
+        System.out.println("\nINMUEBLES ORDENADOS POR PRECIO ALQUILER (ascendente):");
+        mostrarInmuebles(inmuebles);
+
+        // Ordenar por precio de venta ascendente
+        sortPrecioVentaAsc(inmuebles);
+        System.out.println("\nINMUEBLES ORDENADOS POR PRECIO VENTA (ascendente):");
+        mostrarInmuebles(inmuebles);
     }
 }
