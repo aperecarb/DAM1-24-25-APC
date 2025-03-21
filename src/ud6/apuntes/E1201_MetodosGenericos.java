59a6933008fd25d8d3c07ea689b000a943dd224d
Alvaro aperecarb <aperecarb@gmail.com>
2025-03-21 14:01:37 +0100

    Fixes

--- src/ud6/apuntes/E1201_MetodosGenericos.java
+++ src/ud6/apuntes/E1201_MetodosGenericos.java
@@ -3,14 +3,14 @@ package ud6.apuntes;
 import java.util.Arrays;
 
 public class E1201_MetodosGenericos {
-    public static <U> U[] add(U e, U[] arr) {
-        U[] newArr = Arrays.copyOf(arr, arr.length + 1);
-        newArr[arr.length] = e;
+    public static <U> U[] add(U e, U[] t) {
+        U[] newArr = Arrays.copyOf(t, t.length + 1);
+        newArr[t.length] = e;
         return newArr;
     }
 
-    public static <U> boolean buscar(U e, U[] arr) {
-        for (U element : arr) {
+    public static <U> boolean buscar(U e, U[] t) {
+        for (U element : t) {
             if (element.equals(e)) {
                 return true;
             }
@@ -18,9 +18,9 @@ public class E1201_MetodosGenericos {
         return false;
     }
 
-    public static <U> U[] concatenar(U[] arr1, U[] arr2) {
-        U[] newArr = Arrays.copyOf(arr1, arr1.length + arr2.length);
-        System.arraycopy(arr2, 0, newArr, arr1.length, arr2.length);
+    public static <U> U[] concatenar(U[] t1, U[] t2) {
+        U[] newArr = Arrays.copyOf(t1, t1.length + t2.length);
+        System.arraycopy(t2, 0, newArr, t1.length, t2.length);
         return newArr;
     }
 }
