41e85260be1a2b0624c8184c0138d544a84759bb
Alvaro aperecarb <aperecarb@gmail.com>
2025-03-27 12:24:17 +0100

    Fixes

--- src/ud6/ejercicios/Cola.java
+++ src/ud6/ejercicios/Cola.java
@@ -1,6 +1,6 @@
 package ud6.ejercicios;
 
 public interface Cola<T> {
-    void encolar(T elemento);
+    void encolar(T e);
     T desencolar();
 }
--- src/ud6/ejercicios/Pila.java
+++ src/ud6/ejercicios/Pila.java
@@ -1,6 +1,6 @@
 package ud6.ejercicios;
 
 public interface Pila<T> {
-    void apilar(T elemento);
+    void apilar(T e);
     T desapilar();
 }
