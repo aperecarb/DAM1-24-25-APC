17246f42d968ee4e86a6f1b3b165086410664d5f
Alvaro aperecarb <aperecarb@gmail.com>
2025-03-27 12:10:38 +0100

    Fixes

--- src/ud6/ejercicios/ContenedorLista.java
+++ src/ud6/ejercicios/ContenedorLista.java
@@ -2,65 +2,65 @@ package ud6.ejercicios;
 
 public class ContenedorLista<T extends Comparable<T>> implements Pila<T>, Cola<T> {
     private T[] elementos;
-    private int tamanio;
+    private int size;
 
     public ContenedorLista() {
         elementos = (T[]) new Comparable[0];
-        tamanio = 0;
+        size = 0;
     }
 
     public void insertarAlPrincipio(T nuevo) {
-        T[] nuevaTabla = (T[]) new Comparable[tamanio + 1];
+        T[] nuevaTabla = (T[]) new Comparable[size + 1];
         nuevaTabla[0] = nuevo;
-        for (int i = 0; i < tamanio; i++) {
+        for (int i = 0; i < size; i++) {
             nuevaTabla[i + 1] = elementos[i];
         }
         elementos = nuevaTabla;
-        tamanio++;
+        size++;
     }
 
     public void insertarAlFinal(T nuevo) {
-        T[] nuevaTabla = (T[]) new Comparable[tamanio + 1];
-        for (int i = 0; i < tamanio; i++) {
+        T[] nuevaTabla = (T[]) new Comparable[size + 1];
+        for (int i = 0; i < size; i++) {
             nuevaTabla[i] = elementos[i];
         }
-        nuevaTabla[tamanio] = nuevo;
+        nuevaTabla[size] = nuevo;
         elementos = nuevaTabla;
-        tamanio++;
+        size++;
     }
 
     public T extraerDelPrincipio() {
-        if (tamanio == 0) {
+        if (size == 0) {
             return null;
         }
         T elementoExtraido = elementos[0];
-        T[] nuevaTabla = (T[]) new Comparable[tamanio - 1];
-        for (int i = 1; i < tamanio; i++) {
+        T[] nuevaTabla = (T[]) new Comparable[size - 1];
+        for (int i = 1; i < size; i++) {
             nuevaTabla[i - 1] = elementos[i];
         }
         elementos = nuevaTabla;
-        tamanio--;
+        size--;
         return elementoExtraido;
     }
 
     public T extraerDelFinal() {
-        if (tamanio == 0) {
+        if (size == 0) {
             return null;
         }
-        T elementoExtraido = elementos[tamanio - 1];
-        T[] nuevaTabla = (T[]) new Comparable[tamanio - 1];
-        for (int i = 0; i < tamanio - 1; i++) {
+        T elementoExtraido = elementos[size - 1];
+        T[] nuevaTabla = (T[]) new Comparable[size - 1];
+        for (int i = 0; i < size - 1; i++) {
             nuevaTabla[i] = elementos[i];
         }
         elementos = nuevaTabla;
-        tamanio--;
+        size--;
         return elementoExtraido;
     }
 
     public void ordenar() {
-        if (tamanio > 1) {
-            for (int i = 0; i < tamanio - 1; i++) {
-                for (int j = 0; j < tamanio - i - 1; j++) {
+        if (size > 1) {
+            for (int i = 0; i < size - 1; i++) {
+                for (int j = 0; j < size - i - 1; j++) {
                     if (elementos[j].compareTo(elementos[j + 1]) > 0) {
                         T temp = elementos[j];
                         elementos[j] = elementos[j + 1];
@@ -73,14 +73,14 @@ public class ContenedorLista<T extends Comparable<T>> implements Pila<T>, Cola<T
 
     @Override
     public String toString() {
-        if (tamanio == 0) {
+        if (size == 0) {
             return "[]";
         }
         StringBuilder sb = new StringBuilder("[");
-        for (int i = 0; i < tamanio - 1; i++) {
+        for (int i = 0; i < size - 1; i++) {
             sb.append(elementos[i]).append(", ");
         }
-        sb.append(elementos[tamanio - 1]).append("]");
+        sb.append(elementos[size - 1]).append("]");
         return sb.toString();
     }
 
