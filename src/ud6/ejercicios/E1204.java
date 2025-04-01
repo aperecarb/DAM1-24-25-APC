190fe097e06541a76299b9816d60f22d123179bf
aperecarb <aperecarb@gmail.com>
2025-04-01 13:31:15 +0200

    Updates

--- src/ud6/ejercicios/E1204.java
+++ src/ud6/ejercicios/E1204.java
@@ -1,5 +1,41 @@
 package ud6.ejercicios;
 
+import java.util.ArrayList;
+import java.util.Iterator;
+import java.util.List;
+import java.util.Scanner;
+
 public class E1204 {
-    
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        List<Integer> numeros = new ArrayList<>();
+
+        System.out.println("Introduce números enteros no negativos (-1 para terminar):");
+        int numero = scanner.nextInt();
+        while (numero != -1) {
+            numeros.add(numero);
+            numero = scanner.nextInt();
+        }
+
+        System.out.println("Colección: " + numeros);
+
+        System.out.println("Números pares:");
+        for (int num : numeros) {
+            if (num % 2 == 0) {
+                System.out.print(num + " ");
+            }
+        }
+        System.out.println();
+
+        Iterator<Integer> iterator = numeros.iterator();
+        while (iterator.hasNext()) {
+            if (iterator.next() % 3 == 0) {
+                iterator.remove();
+            }
+        }
+
+        System.out.println("Colección sin múltiplos de 3: " + numeros);
+        scanner.close();
+    }
 }
