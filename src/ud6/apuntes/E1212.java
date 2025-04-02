575234ce7a19b79b9268143515372ee0d556b46a
Alvaro aperecarb <aperecarb@gmail.com>
2025-04-02 13:58:26 +0200

    Updates

--- src/ud6/apuntes/E1212.java
+++ src/ud6/apuntes/E1212.java
@@ -1,5 +1,24 @@
 package ud6.apuntes;
 
+import java.util.Set;
+import java.util.TreeSet;
+
 public class E1212 {
-    
+    static <E> Set<E> union(Set<E> conjunto1, Set<E> conjunto2) {
+        Set<E> union = new TreeSet<>(conjunto1);
+        union.addAll(conjunto2);
+        return union;
+    }
+
+    static <E> Set<E> interseccion(Set<E> conjunto1, Set<E> conjunto2) {
+        Set<E> interseccion = new TreeSet<>(conjunto1);
+        interseccion.retainAll(conjunto2);
+        return interseccion;
+    }
+
+    static <E> Set<E> except(Set<E> conjunto1, Set<E> conjunto2) {
+        Set<E> except = new TreeSet<>(conjunto2);
+        except.removeAll(conjunto1);
+        return except;
+    }
 }
