d0063edd70286758eba2506e4c929afa92d54f2a
Alvaro aperecarb <aperecarb@gmail.com>
2025-04-02 12:58:32 +0200

    Fixes

--- src/ud6/ejercicios/E1210.java
+++ src/ud6/ejercicios/E1210.java
@@ -8,18 +8,15 @@ public class E1210 {
         Random random = new Random();
         List<Integer> lista = new ArrayList<>();
 
-        // Generar 20 números aleatorios entre 1 y 10
         for (int i = 0; i < 20; i++) {
             lista.add(random.nextInt(10) + 1);
         }
 
         System.out.println("Lista original: " + lista);
 
-        // Conjunto sin repeticiones
         Set<Integer> sinRepetir = new HashSet<>(lista);
         System.out.println("Conjunto sin repeticiones: " + sinRepetir);
 
-        // Conjunto con elementos repetidos
         Set<Integer> repetidos = new HashSet<>();
         Set<Integer> unicos = new HashSet<>();
         for (Integer num : lista) {
@@ -29,7 +26,6 @@ public class E1210 {
         }
         System.out.println("Conjunto de repetidos: " + repetidos);
 
-        // Conjunto con elementos únicos
         unicos.removeAll(repetidos);
         System.out.println("Conjunto de únicos: " + unicos);
     }
