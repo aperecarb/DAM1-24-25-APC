9edeb29b8627fb1d9e1edf23eebf3fcfe87247a0
Alvaro aperecarb <aperecarb@gmail.com>
2025-04-23 13:59:17 +0200

    Updates

--- src/ud6/apuntes/E1305.java
+++ src/ud6/apuntes/E1305.java
@@ -1,5 +1,24 @@
 package ud6.apuntes;
 
+import java.util.ArrayList;
+import java.util.List;
+import java.util.function.Function;
+
 public class E1305 {
-    
+    public static <T, V> List<V> transformar(List<T> t, Function<T, V> f) {
+        List<V> resultado = new ArrayList<>();
+        for (T elemento : t) {
+            resultado.add(f.apply(elemento));
+        }
+        return resultado;
+    }
+
+    public static void main(String[] args) {
+        List<Double> numeros = List.of(1.0, 4.0, 9.0, 16.0);
+
+        List<Double> raicesCuadradas = transformar(numeros, Math::sqrt);
+
+        System.out.println("Lista original: " + numeros);
+        System.out.println("Lista de raíces cuadradas: " + raicesCuadradas);
+    }
 }
