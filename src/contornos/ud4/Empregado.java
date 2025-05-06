b25f76504f3322c90bc68f3e8a8136e20cac688a
Alvaro aperecarb <aperecarb@gmail.com>
2025-05-06 12:51:21 +0200

    Fixes

--- src/contornos/ud4/Empregado.java
+++ src/contornos/ud4/Empregado.java
@@ -8,7 +8,7 @@ package contornos.ud4;
  * 
  * @see <a href="http://www.google.com">Google</a>
  * @version 1-2025
- * @author NAO
+ * @author APC
  * @since o 1-5-2025
  */
 public class Empregado {
