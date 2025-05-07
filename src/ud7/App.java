8101dc96608b71c98ab0323886f51f9cc8efa2a3
Alvaro aperecarb <aperecarb@gmail.com>
2025-05-07 13:30:36 +0200

    Updates

---
+++ src/ud7/App.java
@@ -0,0 +1,10 @@
+package ud7;
+
+import javafx.*;
+
+public class App {
+    public static void main(String[] args) throws Exception {
+        System.out.println("Hello, JavaFX!");
+        launch();
+    }
+}
