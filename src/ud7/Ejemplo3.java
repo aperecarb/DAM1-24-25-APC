e18e5cee2ef8d5e35d9a2d9c53ea83a91b2b694c
Alvaro aperecarb <aperecarb@gmail.com>
2025-05-08 11:27:21 +0200

    Fixes

--- src/ud7/Ejemplo3.java
+++ src/ud7/Ejemplo3.java
@@ -1,6 +1,5 @@
 package ud7;
 
-
 import javafx.application.Application;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
@@ -9,25 +8,22 @@ import javafx.scene.control.TextField;
 import javafx.scene.layout.VBox;
 import javafx.stage.Stage;
 
-public class Ejemplo3 extends Application{
+public class Ejemplo3 extends Application {
+    TextField txtNum1 = new TextField("Escribe un número:");
+    TextField txtNum2 = new TextField("Escribe otro número:");
+    Button btnSum = new Button("Sumar");
+    Label lblRes = new Label();
+
     public static void main(String[] args) {
         launch(args);
     }
 
     @Override
     public void start(Stage primaryStage) throws Exception {
-        TextField txtNum1 = new TextField("Escribe un número:");
-        TextField txtNum2 = new TextField("Escribe otro número:");
-        Button btnSum = new Button("Sumar");
-        Label lblRes = new Label();
+        txtNum1.setOnKeyTyped(e -> sumar());
+        txtNum2.setOnKeyTyped(e -> sumar());
 
-        btnSum.setOnAction(e -> {
-            int resultado;
-            int n1 = Integer.parseInt(txtNum1.getText());
-            int n2 = Integer.parseInt(txtNum2.getText());
-            resultado = n1 + n2;
-            lblRes.setText(String.valueOf(resultado));
-        });
+        btnSum.setOnAction(e -> sumar());
 
         VBox vbox = new VBox(txtNum1, txtNum2, btnSum, lblRes);
 
@@ -38,5 +34,18 @@ public class Ejemplo3 extends Application{
         primaryStage.setResizable(false);
         primaryStage.centerOnScreen();
         primaryStage.show();
+    }
+
+    private void sumar() {
+        int resultado;
+
+        try {
+            int n1 = Integer.parseInt(txtNum1.getText());
+            int n2 = Integer.parseInt(txtNum2.getText());
+            resultado = n1 + n2;
+            lblRes.setText(String.valueOf(resultado));
+        } catch (Exception ex) {
+            lblRes.setText("ERROR: Algún operando no es un número.");
+        }
     }
 }
