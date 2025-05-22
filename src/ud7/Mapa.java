e769c4b4ea4288dbb82422d1f0b683688554a259
Alvaro aperecarb <aperecarb@gmail.com>
2025-05-22 12:44:02 +0200

    Updates

--- src/ud7/Mapa.java
+++ src/ud7/Mapa.java
@@ -1,6 +1,10 @@
 package ud7;
 
 import javafx.application.Application;
+import javafx.geometry.Pos;
+import javafx.scene.Scene;
+import javafx.scene.control.Label;
+import javafx.scene.layout.GridPane;
 import javafx.stage.Stage;
 
 /*
@@ -26,6 +30,20 @@ public class Mapa extends Application {
 
     @Override
     public void start(Stage primaryStage) throws Exception {
-        
+        GridPane gridPane = new GridPane();
+        for (int i = 0; i < mapa.length; i++) {
+            for (int j = 0; j < mapa[i].length(); j++) {
+                Label lbl = new Label(String.valueOf(mapa[i].charAt(j)));
+                lbl.setPrefSize(50, 50);
+                lbl.setAlignment(Pos.CENTER);
+                lbl.setStyle("-fx-background-color: #151515; -fx-text-fill: white; -fx-border-color: white;");
+                gridPane.add(lbl, j, i);
+            }
+        }
+
+        primaryStage.setTitle("Mapa");
+        primaryStage.setResizable(false);
+        primaryStage.setScene(new Scene(gridPane));
+        primaryStage.show();
     }
 }
