3d3bef9fab3ced2c7b6524fe6d85022d719ea31c
Alvaro aperecarb <aperecarb@gmail.com>
2025-05-22 12:10:11 +0200

    Updates

---
+++ src/ud7/SopaDeLetras.java
@@ -0,0 +1,51 @@
+package ud7;
+
+import javafx.application.Application;
+import javafx.geometry.Pos;
+import javafx.scene.Scene;
+import javafx.scene.control.Label;
+import javafx.scene.layout.GridPane;
+import javafx.stage.Stage;
+
+public class SopaDeLetras extends Application {
+
+    static char[][] sopaLetras = {
+            { 's', 'o', 'z', 'h', 'i' },
+            { 's', 'e' },
+            { 'p', 'o', 'v', 'h' },
+            { 'p', 'o', 'z', 'e' },
+            { 'p', 'o', 'z', 'h', 'n' }
+    };
+
+    @Override
+    public void start(Stage primaryStage) throws Exception {
+        primaryStage.setTitle("Sopa de Letras");
+
+        GridPane gridpane = new GridPane();
+        for (int i = 0; i < sopaLetras.length; i++) {
+            for (int j = 0; j < sopaLetras[i].length; j++) {
+                Label lblLetra = new Label(String.valueOf(sopaLetras[i][j]));
+                lblLetra.setPrefSize(40, 40);
+                lblLetra.setAlignment(Pos.CENTER);
+                gridpane.add(lblLetra, j, i);
+                
+                final int row = i;
+                final int col = j;
+                lblLetra.setOnMouseClicked(e -> {
+                    System.out.println("Has pulsado la letra " + sopaLetras[row][col]);
+                    System.out.println("Fila:" + row);
+                    System.out.println("Columna: " + col);
+                    System.out.println();
+                });
+            }
+        }
+
+        primaryStage.setScene(new Scene(gridpane));
+        primaryStage.setResizable(false);
+        primaryStage.show();
+    }
+
+    public static void main(String[] args) {
+        launch(args);
+    }
+}
